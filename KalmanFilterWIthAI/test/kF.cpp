4bb14432d83fbd5f3d02105c83b7878ffd466937
Abbas Ogal <132055793+abasahmedd@users.noreply.github.com>
2025-05-23 13:39:32 +0300

    Update kF.cpp

--- KalmanFilterWIthAI/test/kF.cpp
+++ KalmanFilterWIthAI/test/kF.cpp
@@ -1,4 +1,4 @@
-SimpleKalman kf(0.0005, 0.002, 1, 0.5);  // Q, R, P0, X0 (جرّب عدّلها)
+SimpleKalman kf(0.0005, 0.002, 1, 0.5);  // Q, R, P0, X0 
 
 void loop() {
   int packetSize = LoRa.parsePacket();
@@ -10,8 +10,8 @@ void loop() {
     while (LoRa.available()) incoming += (char)LoRa.read();
     
 
-    float rawVal     = incoming.toFloat();        // حوّل النص إلى float
-    float kalmanVal  = kf.update(rawVal);         // فلترة
+    float rawVal     = incoming.toFloat();        
+    float kalmanVal  = kf.update(rawVal);        
     int   rssi       = LoRa.packetRssi();
 
     Serial.printf("RAW=%.2f | KAL=%.2f | RSSI:%d dBm\n",
